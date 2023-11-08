f0afd0ed3cd0c144d661845779cdc459c65f3f34
jordanoski19 <102685979+jordanoski19@users.noreply.github.com>
2023-11-08 05:49:28 +0100

    Animations Optimized

---
+++ scripts/background.js
@@ -0,0 +1,106 @@
+$("document").ready(function () {
+  let x = 0;
+  let y = 50;
+
+  const updateBackgroundPosition = () => {
+    if (x >= 0 && x <= 50 && y <= 50 && y > 0) {
+      x += 0.1;
+      y -= 0.1;
+    } else if (x >= 50 && x <= 100 && y >= 0 && y < 50) {
+      x += 0.1;
+      y += 0.1;
+    } else if (x >= 50 && x <= 100 && y >= 50 && y < 100) {
+      x -= 0.1;
+      y += 0.1;
+    } else if (x <= 50 && x >= 0 && y <= 100 && y > 50) {
+      x -= 0.1;
+      y -= 0.1;
+    }
+
+    x = Number(x.toFixed(2));
+    y = Number(y.toFixed(2));
+
+    $("body").css({
+      backgroundPositionX: x + "%",
+      backgroundPositionY: y + "%",
+    });
+
+    requestAnimationFrame(updateBackgroundPosition);
+  };
+
+  requestAnimationFrame(updateBackgroundPosition);
+
+  function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let starNum = 0;
+  let starSize;
+  let starSpeed;
+
+  function createStar() {
+    starSize = randomInteger(1, 9) / randomInteger(1, 7);
+    let starId = "star" + starNum;
+
+    $("#stars").append("<span id='" + starId + "' class='star'></span");
+
+    $("#" + starId).css({
+      width: starSize,
+      height: starSize * 0.75,
+      boxShadow:
+        "0 0 " +
+        starSize * 2 +
+        "px " +
+        starSize +
+        "px rgb(" +
+        randomInteger(1, 255) +
+        "," +
+        randomInteger(1, 255) +
+        ", " +
+        randomInteger(1, 255) +
+        ")",
+      top: randomInteger(-10, 110) + "%",
+      left: randomInteger(-10, 110) + "%",
+      transform: "rotate(" + randomInteger(1, 360) + "deg)",
+    });
+
+    if (starSize <= 4) {
+      starSpeed = randomInteger(6000, 10500);
+    } else {
+      starSpeed = randomInteger(2500, 3500);
+    }
+
+    $("#" + starId).transition(
+      {
+        rotate: "+=" + randomInteger(1, 10) + "deg",
+        x: -2500,
+        y: 0,
+      },
+      {
+        duration: starSpeed,
+        easing: "linear",
+        queue: false,
+        complete: function () {
+          $(this).remove();
+        },
+      }
+    );
+
+    starNum += 1;
+  }
+
+  let lastUpdate = performance.now();
+
+  function animate(time) {
+    let deltaTime = time - lastUpdate;
+
+    if (deltaTime >= 500) {
+      createStar();
+      lastUpdate = time;
+    }
+
+    requestAnimationFrame(animate);
+  }
+
+  requestAnimationFrame(animate);
+});
---
+++ scripts/main.js
@@ -0,0 +1,150 @@
+$("document").ready(function () {
+  $("#navigation ul li").hide().slideDown(1000);
+  $("#hello").hide().fadeIn(850);
+
+  let bounceInOneElement = 1;
+  $("#aboutMeButton").on("click", function () {
+    $("#skills, #hello, #hideAll").fadeOut();
+    $(
+      ".indexDots, .nextB, .previousB, .theButton, .bouncez" +
+        bounceInOneElement
+    ).hide();
+    $(".aboutMeP, .pContainer").show();
+    retextillate(".aboutMeP", "bounceIn", "none", "in");
+  });
+
+  $("#projectsButton").on("click", function () {
+    $("#hello, #skills").fadeOut();
+    retextillate(".aboutMeP", "none", "fadeOutDown", "out");
+    $(
+      "#projects, .pContainer, .indexDots, .nextB, .previousB, .theButton, .bouncez" +
+        bounceInOneElement
+    ).show();
+    $("#hideAll").slideDown();
+    retextillate(
+      ".bouncez" + bounceInOneElement + "",
+      "bounceIn",
+      "none",
+      "in"
+    );
+  });
+
+  $("#skillsButton").on("click", function () {
+    $("#hello, #hideAll").fadeOut();
+    $(".pContainer").hide();
+    $("#skills").fadeIn();
+  });
+
+  $("#hideAll").on("click", function () {
+    $("header, #main").fadeOut("slow");
+    $("#showAll").fadeIn(1200);
+  });
+
+  $("#showAll").on("click", function () {
+    $(this).fadeOut(300);
+    $("header, #main").fadeIn("slow");
+  });
+
+  let tabs = $(".projectsTab");
+  for (let i = 0; i < tabs.length; i++) {
+    $(tabs[i]).append(
+      "<div id='buttonHandler'><div class='indexDots'></div><button class='previousB'><</button><button class='nextB'>></button></div>"
+    );
+    $(tabs[i]).css({
+      left: i * 100 + "%",
+    });
+  }
+  for (let i = 0; i < tabs.length; i++) {
+    $(".indexDots").append("<span class='indexDot' numberId=" + i + "></span>");
+  }
+
+  let indexDots = $(".indexDot");
+
+  $(indexDots[0]).css("background", "#03e9f4");
+
+  let currentPosition = 0;
+
+  indexDots.on("click", function (event) {
+    currentPosition = -Math.abs(event.target.getAttribute("numberid"));
+    for (var i = 0; i < tabs.length; i++) {
+      $(tabs[i]).animate({
+        left: (currentPosition + i) * 200 + "%",
+      });
+    }
+    $(indexDots).css("background", "transparent");
+    $(".indexDot[numberid='" + Math.abs(currentPosition) + "']").css(
+      "background",
+      "#03e9f4"
+    );
+    bounceInOneElement = Number(event.target.getAttribute("numberid")) + 1;
+  });
+
+  $(".nextB").on("click", function () {
+    currentPosition--;
+    if (currentPosition <= -tabs.length + 1) {
+      currentPosition = -tabs.length + 1;
+    }
+    for (var i = 0; i < tabs.length; i++) {
+      $(tabs[i]).animate({
+        left: (currentPosition + i) * 200 + "%",
+      });
+    }
+    $(indexDots).css("background", "transparent");
+    $(".indexDot[numberid='" + Math.abs(currentPosition) + "']").css(
+      "background",
+      "#03e9f4"
+    );
+    bounceInOneElement = currentPosition * -1 + 1;
+  });
+
+  $(".previousB").on("click", function () {
+    currentPosition++;
+    if (currentPosition >= 0) {
+      currentPosition = 0;
+    }
+    for (var i = 0; i < tabs.length; i++) {
+      $(tabs[i]).animate({
+        left: (currentPosition + i) * 200 + "%",
+      });
+    }
+    $(indexDots).css("background", "transparent");
+    $(".indexDot[numberid='" + Math.abs(currentPosition) + "']").css(
+      "background",
+      "#03e9f4"
+    );
+    bounceInOneElement = currentPosition * -1 + 1;
+  });
+});
+
+function retextillate(element, in_effect = "", out_effect = "", type) {
+  $(element).textillate({
+    loop: true,
+    autoStart: false,
+    in: {
+      effect: in_effect,
+      delay: 0.7,
+      shuffle: true,
+    },
+    out: {
+      effect: out_effect,
+      reverse: true,
+      delay: 0.5,
+      shuffle: true,
+      callback: function () {
+        $(element).css("opacity", 0);
+      },
+    },
+  });
+
+  if (type == "in") {
+    $(element).textillate("in");
+    $(element).css("opacity", 1);
+  } else if (type == "out") {
+    $(element).textillate("out");
+  } else if (type == "both") {
+    $(element).textillate("in");
+    setTimeout(function () {
+      $(element).textillate("out");
+    }, 1000);
+  }
+}
